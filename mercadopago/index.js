8be0ac5c3ad6f47c16b8ee4c62b21b6be36b10e5
GonzaloHaag <111447167+GonzaloHaag@users.noreply.github.com>
2023-07-04 18:48:59 -0300

    Modificando archivo mp

--- mercadopago/index.js
+++ mercadopago/index.js
@@ -1,19 +1,21 @@
 import express from 'express';
 import morgan from 'morgan';
 import paymentRutas from './src/rutas/payment.rutas.js';
-import {PORT} from './src/config.js';
-
+import { PORT } from './src/config.js';
 import cors from 'cors';
 
 const app = express();
+
 app.use(cors());
-
 app.use(express.json());
+app.use(morgan('dev'));
 
-app.use(morgan('dev')); //Da informacion sobre las peticiones en mi consola
-app.get('/',(req,res)=> {
-    res.json("Hello");
-})
+app.get('/', (req, res) => {
+  res.json("Hello");
+});
+
 app.use(paymentRutas);
-app.listen(PORT);
-console.log('SERVER EN PUERTO: ' , PORT);
+
+app.listen(PORT, () => {
+  console.log('SERVER EN PUERTO:', PORT);
+});
