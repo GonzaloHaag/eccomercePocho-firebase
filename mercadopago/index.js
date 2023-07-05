e49881dc0666f85738a16b7d3b23c048c8af21b4
GonzaloHaag <111447167+GonzaloHaag@users.noreply.github.com>
2023-07-05 10:33:05 -0300

    Volviendo a configurar el acces control

--- mercadopago/index.js
+++ mercadopago/index.js
@@ -8,8 +8,10 @@ import { rutaPrincipal } from './src/controllers/payment.controller.js';
 const app = express();
 
 app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://eccomerce-pocho-firebase-frontend.vercel.app');
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 app.use(express.json());
